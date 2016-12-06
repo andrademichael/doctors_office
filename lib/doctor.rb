9a1413b85d5f5e85a6ccf00bd65ece118d40f951
Michael Andrade and Marty Kovach <michael@>
2016-12-06 09:50:40 -0800

    Create Specialty class, add initialize method.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,11 +1,13 @@
 class Doctor
-  attr_reader(:name, :specialty, :id, :patient_ids)
+  attr_reader(:name, :id, :patient_ids)
 
   define_method(:initialize) do |attributes|
         @name = attributes.fetch(:name)
-        @specialty = attributes.fetch(:specialty)
         @id = attributes.fetch(:id)
         @patient_ids = []
   end
 
+  define_method(:assign_patient) do |patient_id|
+    @patient_ids.push(patient_id)
+  end
 end
--- spec/doctors_office_spec.rb
+++ spec/doctors_office_spec.rb
@@ -1,14 +1,23 @@
 require('rspec')
 require('doctor')
 require('patient')
+require('specialty')
+
+test_patient = Patient.new({:name => "Tom", :birthdate => "1899-05-12", :id=> nil})
+test_doctor = Doctor.new({:name => "Tim", :id => nil})
+test_specialty = Specialty.new({:name => "Backrubiatry", :id => nil})
 
 describe(Doctor) do
   describe(:initialize) do
     it('creates a new Doctor with name and specialty') do
-      test_doctor = Doctor.new({:name => "Tim", :specialty => "backrubs", :id => nil})
       expect(test_doctor.name()).to(eq("Tim"))
-      expect(test_doctor.specialty()).to(eq("backrubs"))
       expect(test_doctor.id()).to(eq(nil))
+    end
+  end
+  describe(:assign_patient) do
+    it('adds a patients id to the doctors patient_ids array') do
+      test_doctor.assign_patient(test_patient)
+      expect(test_doctor.patient_ids()).to(eq([test_patient]))
     end
   end
 end
@@ -16,10 +25,18 @@ end
 describe(Patient) do
   describe(:initialize) do
     it('creates a new Patient with name and birthdate') do
-      test_patient = Patient.new({:name => "Tom", :birthdate => "1899-05-12", :id=> nil})
       expect(test_patient.name()).to(eq("Tom"))
       expect(test_patient.birthdate()).to(eq("1899-05-12"))
       expect(test_patient.id()).to(eq(nil))
+    end
+  end
+end
+
+describe(Specialty) do
+  describe(:initialize) do
+    it('creates a new Specialty with name and array of doctor ids') do
+      expect(test_specialty.name()).to(eq("Backrubiatry"))
+      expect(test_specialty.id()).to(eq(nil))
     end
   end
 end
