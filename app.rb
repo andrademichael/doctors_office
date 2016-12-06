79431d80b45436d05c9599d648074986d40156e1
Michael Andrade and Marty Kovach <michael@>
2016-12-06 14:54:52 -0800

    passing itegration spec for doctor info post path

--- app.rb
+++ app.rb
@@ -7,10 +7,20 @@ require('./lib/specialty.rb')
 require('./lib/patient.rb')
 also_reload('lib/**/*.rb')
 
+DB = PG.connect({:dbname => 'doctors_office_test'})
+
 get("/") do
   erb(:index)
 end
 
 get("/add_doctor") do
   erb(:add_doctor)
+end
+
+post("/success") do
+  doctor_name = params.fetch('doctor_name_input')
+  doctor_specialty = params.fetch('doctor_specialty_input')
+  doctor = Doctor.new({:name => doctor_name, :id => nil})
+  doctor.save()
+  erb(:success)
 end
--- spec/doctors_office_integration_spec.rb
+++ spec/doctors_office_integration_spec.rb
@@ -10,3 +10,13 @@ describe("to the add doctor path", {:type => :feature}) do
     expect(page).to have_content("Doctor's Name")
   end
 end
+
+describe("the add doctor path", {:type => :feature}) do
+  it('takes user input for doctor name, and specialty')do
+    visit('/add_doctor')
+    fill_in('doctor_name_input', :with => "Tim")
+    fill_in('doctor_specialty_input', :with => "Backrubiatry")
+    click_button('Add the Doc!')
+    expect(page).to have_content("Your information has been added!")
+  end
+end
