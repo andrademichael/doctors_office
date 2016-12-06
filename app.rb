13b4fe321d0e1cffc314ec2dc35ca3960b3d2289
Michael Andrade and Marty Kovach <michael@>
2016-12-06 14:32:57 -0800

    add integration spec to navigate to add doctor page.

--- app.rb
+++ app.rb
@@ -1,13 +1,16 @@
 require('sinatra')
 require('sinatra/reloader')
 require('pry')
-require('rspec')
-require('capybara')
 require('pg')
-require('.\app.rb')
-require('lib\*.rb')
+require('./lib/doctor.rb')
+require('./lib/specialty.rb')
+require('./lib/patient.rb')
 also_reload('lib/**/*.rb')
 
 get("/") do
   erb(:index)
+end
+
+get("/add_doctor") do
+  erb(:add_doctor)
 end
--- spec/doctors_office_integration_spec.rb
+++ spec/doctors_office_integration_spec.rb
@@ -3,12 +3,10 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe("to the add doctor path") do
+describe("to the add doctor path", {:type => :feature}) do
   it('the path user takes to get to the add_doctor form') do
     visit('/')
-    click_link('add a doctor')
-    expect(page).to have_content("doctor's name")
-
-
+    click_on('Add a new doctor')
+    expect(page).to have_content("Doctor's Name")
   end
 end
