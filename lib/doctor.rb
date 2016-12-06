5ec833666646819fbd779f78fd8529318aa61ac7
Michael Andrade and Marty Kovach <marty@>
2016-12-06 10:33:10 -0800

    add .all method for Doctor, begin sql database integration.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -10,4 +10,15 @@ class Doctor
   define_method(:assign_patient) do |patient_id|
     @patient_ids.push(patient_id)
   end
+
+  define_singleton_method(:all) do
+    returned_doctors = DB.exec("SELECT * FROM doctors;")
+    doctors = []
+    returned_doctors.each() do |doctor|
+      name = doctor.fetch("name")
+      doctors.push(Doctor.new({:name => name}))
+    end
+    doctors
+  end
+
 end
--- spec/doctors_office_spec.rb
+++ spec/doctors_office_spec.rb
@@ -2,28 +2,49 @@ require('rspec')
 require('doctor')
 require('patient')
 require('specialty')
+require('pg')
+#
+DB = PG.connect({:dbname => 'doctors_office_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM doctors;")
+  end
+end
 
 test_patient = Patient.new({:name => "Tom", :birthdate => "1899-05-12", :id=> nil})
 test_doctor = Doctor.new({:name => "Tim", :id => nil})
 test_specialty = Specialty.new({:name => "Backrubiatry", :id => nil})
 
 describe(Doctor) do
-  describe(:initialize) do
+  describe('#initialize') do
     it('creates a new Doctor with name and specialty') do
       expect(test_doctor.name()).to(eq("Tim"))
       expect(test_doctor.id()).to(eq(nil))
     end
   end
-  describe(:assign_patient) do
+  describe("#assign_patient") do
     it('adds a patients id to the doctors patient_ids array') do
       test_doctor.assign_patient(test_patient)
       expect(test_doctor.patient_ids()).to(eq([test_patient]))
     end
   end
+
+  describe(".all") do
+    it('will return an empty array at first') do
+      expect(Doctor.all()).to(eq([]))
+    end
+  end
+
+  # describe("#save") do
+  #   it('will save the doctor to the database')do
+  #
+  #   end
+  # end
 end
 
 describe(Patient) do
-  describe(:initialize) do
+  describe('#initialize') do
     it('creates a new Patient with name and birthdate') do
       expect(test_patient.name()).to(eq("Tom"))
       expect(test_patient.birthdate()).to(eq("1899-05-12"))
@@ -33,7 +54,7 @@ describe(Patient) do
 end
 
 describe(Specialty) do
-  describe(:initialize) do
+  describe('#initialize') do
     it('creates a new Specialty with name and array of doctor ids') do
       expect(test_specialty.name()).to(eq("Backrubiatry"))
       expect(test_specialty.id()).to(eq(nil))
